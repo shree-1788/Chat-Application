0c39ec604ed1cb61fc9bc796c34f45b862ece9f1
shree-1788 <shreepatil1788@gmail.com>
2023-06-26 21:37:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require("path");
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.resolve("./public/index.html"));
+});
+
+// sockets
+io.on("connection", (socket) => {
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server is running on port no. 3000");
+});
